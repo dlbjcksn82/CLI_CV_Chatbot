7b1e69c4091fbc2f024f32b085655e760ccce357
dlbjcksn82 <dj918@york.ac.uk>
2025-03-18 09:34:41 +0000

    Added code to run the LLM on Apple GPU's MPS (Metal Performance Shaders)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import sys
 
+#optimize torch for Apple computers
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+
 # Load the model & tokenizer
 print("Starting Code")
 MODEL_NAME = "google/gemma-3-1b-pt"
@@ -9,27 +12,32 @@ print("Loading Tokenizer")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 print("Loading Model")
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.float32)
+model.to(DEVICE)
 print("Model Loaded")
+
+
+
+
 # Function to generate text
-
-
 def chat_with_gemma(prompt):
     print("Getting a response from the LLM......")
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(
-        **inputs,
-        max_length=150,
-        do_sample=True,
-        temperature=0.5,  # controls randomness
-        top_p=0.9,  # nucleus sampling to balance diversity
-        repetition_penalty=1.2  # Penalizes word repetition
-        )
+    inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
+    with torch.no_grad():
+        outputs = model.generate(
+            **inputs,
+            max_length=150,
+            do_sample=True,
+            temperature=0.5,  # controls randomness
+            top_p=0.9,  # nucleus sampling to balance diversity
+            repetition_penalty=1.2  # Penalizes word repetition
+            )
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     print("\nGemma's Response:\n", response)
 
+
 print("Starting up chat")
 print("Type the word quit to end the program")
-# Example
+# Loop to enable the user to continually chat
 runLLM = True
 while runLLM:
     try:
